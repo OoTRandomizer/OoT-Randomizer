199087c4e942d61118957e5d75fdc7de445de1fd
Accept86 <accept8605@gmail.com>
2019-04-28 18:56:53 +0200

    cleaning up

--- Entrance.py
+++ Entrance.py
@@ -3,31 +3,38 @@ class Entrance(object):
     def __init__(self, name='', parent=None):
         self.name = name
         self.parent_region = parent
+        self.world = parent.world
         self.connected_region = None
-        self.addresses = None
         self.spot_type = 'Entrance'
         self.recursion_count = { 'child': 0, 'adult': 0 }
         self.access_rule = lambda state: True
-        self.world = None
+        self.reverse = None
+        self.replaces = None
+        self.assumed = None
         self.type = None
         self.shuffled = False
+        self.data = None
+        self.primary = False
 
 
     def copy(self, new_region):
         new_entrance = Entrance(self.name, new_region)
-        new_entrance.world = new_region.world
         new_entrance.connected_region = self.connected_region.name
-        new_entrance.addresses = self.addresses
         new_entrance.spot_type = self.spot_type
         new_entrance.access_rule = self.access_rule
+        new_entrance.reverse = self.reverse
+        new_entrance.replaces = self.replaces
+        new_entrance.assumed = self.assumed
         new_entrance.type = self.type
         new_entrance.shuffled = self.shuffled
+        new_entrance.data = self.data
+        new_entrance.primary = self.primary
 
         return new_entrance
 
 
     def can_reach(self, state, noparent=False):
-        return state.with_spot(self.access_rule, spot=self) and (noparent or state.can_reach(self.parent_region))
+        return state.with_spot(self.access_rule, spot=self) and (noparent or state.can_reach(self.parent_region, keep_tod=True))
 
 
     def connect(self, region):
