2d4773ca564fa7f28b995b1cb1d7aa8d532ab49f
Ross Levine <rlbond86@gmail.com>
2020-01-04 11:05:17 -0700

    add fade in/out to agony marker

--- ASM/c/agony.c
+++ ASM/c/agony.c
@@ -4,11 +4,13 @@
 static signed char amp[20] =  {1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1};
 static signed char sign[20] = {1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1};
 
-#define DIST_THRESHOLD 200
+#define DIST_THRESHOLD 190
 #define DIST_MULT (0xFFFFFFFF / DIST_THRESHOLD)
+#define ALPHA_STEP 0x10
 
 static unsigned int best_dist = DIST_THRESHOLD + 1;
-static unsigned int agony_ticks = 0;
+static unsigned char agony_ticks = 0;
+static unsigned char agony_alpha = 0;
 
 void update_agony_distance(z64_actor_t* grotto) {
     unsigned int xzdist = (unsigned int)grotto->xzdist_from_link;
@@ -23,16 +25,34 @@ static void draw_agony_graphic(int offset) {
     gSPDisplayList(db->p++, &setup_db);
     gDPPipeSync(db->p++);
     gDPSetCombineMode(db->p++, G_CC_MODULATEIA_PRIM, G_CC_MODULATEIA_PRIM);
-    uint16_t alpha = 0xFF;
-    gDPSetPrimColor(db->p++, 0, 0, 0xFF, 0xFF, 0xFF, alpha);
+    gDPSetPrimColor(db->p++, 0, 0, 0xFF, 0xFF, 0xFF, agony_alpha);
     sprite_load(db, &quest_items_sprite, 9, 1);
     sprite_draw(db, &quest_items_sprite, 0, 26+offset, 190, 16, 16);
 
     gDPPipeSync(db->p++);
 }
 
+static void increase_alpha() {
+    int alpha = (int)agony_alpha;
+    alpha += ALPHA_STEP;
+    if (alpha > 0xFF) {
+        alpha = 0xFF;
+    }
+    agony_alpha = (unsigned char)alpha;
+}
+
+static void decrease_alpha() {
+    int alpha = (int)agony_alpha;
+    alpha -= ALPHA_STEP;
+    if (alpha < 0) {
+        alpha = 0;
+    }
+    agony_alpha = (unsigned char)alpha;
+}
+
 void draw_agony() {
     if (best_dist <= DIST_THRESHOLD) {
+        increase_alpha();
         unsigned int amp_reduction = (best_dist * DIST_MULT) >> 29; // 0-7
         int amplitude = amp[agony_ticks] - amp_reduction;
         if (amplitude < 0) {
@@ -47,6 +67,10 @@ void draw_agony() {
     }
     else {
         agony_ticks = 0;
+        decrease_alpha();
+        if (agony_alpha > 0) {
+            draw_agony_graphic(0);
+        }
     }
 }
 
