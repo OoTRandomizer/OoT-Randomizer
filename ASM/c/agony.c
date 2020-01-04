83e73b35fdb6ae97c31dc02b10f0ef1cd66c05f1
Ross Levine <rlbond86@gmail.com>
2020-01-03 22:20:36 -0700

    match rumble distance

--- ASM/c/agony.c
+++ ASM/c/agony.c
@@ -4,7 +4,7 @@
 static signed char amp[20] =  {1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1};
 static signed char sign[20] = {1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1};
 
-#define DIST_THRESHOLD 300
+#define DIST_THRESHOLD 200
 #define DIST_MULT (0xFFFFFFFF / DIST_THRESHOLD)
 
 static unsigned int best_dist = DIST_THRESHOLD + 1;
@@ -12,8 +12,8 @@ static unsigned int agony_ticks = 0;
 
 void update_agony_distance(z64_actor_t* grotto) {
     unsigned int xzdist = (unsigned int)grotto->xzdist_from_link;
-    unsigned int ydist = (unsigned int)grotto->ydist_from_link;
-    if (xzdist < best_dist && ydist <= DIST_THRESHOLD) {
+    unsigned int distsq = (unsigned int)grotto->distsq_from_link;
+    if (xzdist < best_dist && distsq <= DIST_THRESHOLD * DIST_THRESHOLD) {
         best_dist = xzdist;
     }
 }
@@ -26,7 +26,7 @@ static void draw_agony_graphic(int offset) {
     uint16_t alpha = 0xFF;
     gDPSetPrimColor(db->p++, 0, 0, 0xFF, 0xFF, 0xFF, alpha);
     sprite_load(db, &quest_items_sprite, 9, 1);
-    sprite_draw(db, &quest_items_sprite, 0, 13, 206+offset, 16, 16);
+    sprite_draw(db, &quest_items_sprite, 0, 26+offset, 190, 16, 16);
 
     gDPPipeSync(db->p++);
 }
--- ASM/c/agony.h
+++ ASM/c/agony.h
@@ -3,7 +3,7 @@
 
 #include "z64.h"
 
-void update_agony_distance(z64_actor_t* distance_squared);
+void update_agony_distance(z64_actor_t* grotto);
 void draw_agony();
 
 #endif
