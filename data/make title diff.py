8680ad13401cd28d93a1ad052a05f9fcb1768bc3
JackTriton <35764777+JackTriton@users.noreply.github.com>
2021-12-12 10:33:58 +0900

    Add files via upload

--- data/make title diff.py
+++ data/make title diff.py
@@ -1,11 +1,11 @@
 import zlib
 
-def make_title(replace, export):
-    Original = open("titleoriginal.bin", "rb").read()
+def make_diff(original, replace, export):
+    Original = open(original, "rb").read()
     New = open(replace, "rb").read()
+    if b"\x00\x00\x00\x00" in New:
+        New = New.replace(b"\x00\x00\x00\x00", b"\xFF\xFF\xFF\x00")
     titleBytes = bytearray([a ^ b for a, b in zip(Original, New)])
     titleDiff = zlib.compress(titleBytes)
     with open(export, "wb") as new:
         new.write(titleDiff)
-
-make_title("titleJP.bin", "JP.bin")
