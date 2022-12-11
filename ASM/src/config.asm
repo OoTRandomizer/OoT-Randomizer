cecf272617079c1cb9161fca435180c283437f69
Fenhl <fenhl@fenhl.net>
2022-12-11 09:09:32 +0000

    Chest Appearance Does Not Match Contents

--- ASM/src/config.asm
+++ ASM/src/config.asm
@@ -162,6 +162,10 @@ PLANDOMIZER_USED:
 .byte 0x00
 POTCRATE_TEXTURES_MATCH_CONTENTS:
 .byte 0x00
+INCORRECT_CHEST_APPEARANCES:
+.byte 0x00
+CFG_GLITCHLESS_LOGIC:
+.byte 0x00
 .align 4
 
 ; These configuration values are given fixed addresses to aid auto-trackers.
