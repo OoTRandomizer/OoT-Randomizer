ac67f379129d148a2878861232ab05cfae950712
Rob Realmuto <rrealmuto@gmail.com>
2025-01-11 23:32:22 -0500

    venv stuff

--- Gui.py
+++ Gui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import platform
 import sys
 if sys.version_info < (3, 8):
     print("OoT Randomizer requires Python version 3.8 or newer and you are using %s" % '.'.join([str(i) for i in sys.version_info[0:3]]))
@@ -13,12 +14,44 @@ if sys.version_info < (3, 8):
 import shutil
 import subprocess
 import webbrowser
+import os
+import venv
+
 
 from SettingsToJson import create_settings_list_json
 from Utils import local_path, data_path, compare_version, VersionError
 
 
 def gui_main() -> None:
+
+    python_path = sys.executable
+    # Get python virtual environment
+    # Make it if it doesn't exist
+
+    print("Checking for python virtual environment")
+    try:
+        if os.path.exists(".venv"):
+            print("Found python virtual environment")
+            # Looks like venv exists so just set the python path
+            pass
+        else:
+            # Virtual environment doesn't exist so create it
+            print("No virtual environment found. Creating...")
+            venv.create(".venv", system_site_packages=False, clear=True, symlinks=False, with_pip=True)
+        if platform.system() == "Windows":
+            subdir = "Scripts"
+            exename = "python.exe"
+        else:
+            subdir = "bin"
+            exename = "python3"
+        args = [os.path.join(".venv", subdir, "pip"), "install", "-r", "requirements.txt"]
+        print("Installing any missing packages")
+        subprocess.run(args, check=True)
+        # Get platform dependent path to python
+        python_path = os.path.abspath(os.path.join(os.curdir, ".venv", subdir, exename))
+    except Exception as err:
+        print(f"Could not create python virtual environment, defaulting to global python interpreter {python_path}")
+
     try:
         version_check("Node", "14.15.0", "https://nodejs.org/en/download/")
         version_check("NPM", "6.12.0", "https://nodejs.org/en/download/")
@@ -34,9 +67,8 @@ def gui_main() -> None:
     if web_version:
         args = ["node", "run.js", "web"]
     else:
-        args = ["node", "run.js", "release", "python", sys.executable]
+        args = ["node", "run.js", "release", "python", python_path]
     subprocess.run(args, shell=False, cwd=local_path("GUI"), check=True)
-
 
 def version_check(name: str, version: str, url: str) -> None:
     try:
