ec917ed87f8199b3d3ff0a95bb470fb702a34173
Fenhl <fenhl@fenhl.net>
2025-03-21 13:06:49 +0000

    Show hint type in spoiler log

--- Hints.py
+++ Hints.py
@@ -92,9 +92,16 @@ class GossipText:
         self.colors: Optional[list[str]] = colors
         self.hinted_locations: Optional[list[str]] = hinted_locations
         self.hinted_items: Optional[list[str]] = hinted_items
+        self.hint_type: Optional[str] = None
 
     def to_json(self) -> dict:
-        return {'text': self.text, 'colors': self.colors, 'hinted_locations': self.hinted_locations, 'hinted_items': self.hinted_items}
+        return {
+            'text': self.text,
+            'colors': self.colors,
+            'hinted_locations': self.hinted_locations,
+            'hinted_items': self.hinted_items,
+            'hint_type': self.hint_type,
+        }
 
     def __str__(self) -> str:
         return get_raw_text(line_wrap(color_text(self)))
@@ -186,7 +193,9 @@ def is_restricted_dungeon_item(item: Item) -> bool:
 
 
 def add_hint(spoiler: Spoiler, world: World, groups: list[list[int]], gossip_text: GossipText, count: int,
-             locations: Optional[list[Location]] = None, force_reachable: bool = False, hint_type: str = None) -> bool:
+             locations: Optional[list[Location]] = None, force_reachable: bool = False, *, hint_type: str) -> bool:
+    gossip_text.hint_type = hint_type
+
     random.shuffle(groups)
     skipped_groups = []
     duplicates = []
