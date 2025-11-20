3384ec5cbcd391a8398bb882d5a04d15c8557401
Rob Realmuto <rrealmuto@gmail.com>
2025-11-19 20:07:56 -0500

    Move adpcm library load into function instead of module load

--- bin/tools/adpcm/adpcm_encode.py
+++ bin/tools/adpcm/adpcm_encode.py
@@ -3,17 +3,18 @@ import os
 import sys
 from binascii import hexlify
 
-# Import the shared library. Need to check for windows vs. linux vs. mac
-shared_lib = "vadpcm_enc_shared"
-if sys.platform == 'darwin':
-    shared_lib = "vadpcm_enc_shared_mac"
-
-try:
-    lib = CDLL(os.path.join(os.path.dirname(os.path.realpath(__file__)),shared_lib))
-except:
-    raise Exception(os.path.curdir)
 # Converts raw 16-bit samples to ADPCM compressed bytes
 def adpcm_encode(frames: bytes, num_frames: int):
+    # Import the shared library. Need to check for windows vs. linux vs. mac
+    shared_lib = "vadpcm_enc_shared"
+    if sys.platform == 'darwin':
+        shared_lib = "vadpcm_enc_shared_mac"
+
+    try:
+        lib = CDLL(os.path.join(os.path.dirname(os.path.realpath(__file__)),shared_lib))
+    except:
+        raise Exception(os.path.curdir)
+
     # Convert params to ctypes
     c_in_buffer = c_char_p(frames)
     c_num_frames = c_int(num_frames)
