a0a168e091443690d723606b9190baad39530293
djevangelia <djevangelia>
2026-04-11 15:32:16 +0200

    Ladder cutscene fix, also include case with cutscene items

--- ASM/src/ladder_cutscene.asm
+++ ASM/src/ladder_cutscene.asm
@@ -1,11 +1,11 @@
 ladder_cutscene:
-    lbu	t4,1693(s0)             ; player->unk_6AD
-    li	t3,3                    ; Cutscene = 3
-    bne	t4,t3,@@ladder_return   ; If not in cutscene, continue as usual
+    lbu   t4,1693(s0)           ; player->unk_6AD
+    addi  t3,t4,-3              ; Cutscene = 3, CS item = 4
+    bltz  t3,@@ladder_return    ; If not CS/CS item, continue as usual
     nop
-    la ra,0x803a3080            ; If in cutscene, continue at 0x8084a6e0 (load argument to LinkAnimation_Update)   
+    la    ra,0x803a3080         ; Else, continue at 0x8084a6e0 (load argument to LinkAnimation_Update)
 
 @@ladder_return:                ; Original code
-    lw  t8,1644(s0)
-    jr  ra
-    lui at,0xffdf
+    lw    t8,1644(s0)
+    jr    ra
+    lui   at,0xffdf
