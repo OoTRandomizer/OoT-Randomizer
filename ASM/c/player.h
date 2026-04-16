e4a3f2f4e7ac08a4fb6041014292ab43d9a1115a
djevangelia <djevangelia>
2026-04-16 11:39:47 +0200

    Fix for Epona spawning when entering scene on water

---
+++ ASM/c/player.h
@@ -0,0 +1,7 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define R_EXITED_SCENE_RIDING_HORSE_addr           0x801c7cf0
+#define R_EXITED_SCENE_RIDING_HORSE                (*(int16_t*) R_EXITED_SCENE_RIDING_HORSE_addr)
+
+#endif
---
+++ ASM/src/hacks/z_player.asm
@@ -0,0 +1,14 @@
+.headersize(0x808301c0 - 0xbcdb70)
+
+;================================================================================
+; Fixes Epona spawning on water when exiting into a water entrance while riding.
+;================================================================================
+; Replaces      sub.s   $f10,$f6,$f8
+;               move    a0,s1
+;               move    a1,s0
+;               swc1    $f10,40(sp)
+.org 0x8083aa1c                     ; in Player_SetStartingMovement (start at 0x80393360)
+    sub.s   $f12,$f6,$f8            ; (waterbox Y surface - player Y) = ySurface
+    move    s7,a2                   ; store Player_Action_StartModeWater address
+    jal     Player_CheckEponaWater  
+    swc1    $f12,40(sp)             ; store new ySurface (restored in end of jal to $f10)
