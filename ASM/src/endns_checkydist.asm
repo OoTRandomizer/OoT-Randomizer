cddc4ef51925f1d3362193091114d605dcbe1265
djevangelia <djevangelia>
2026-04-18 09:14:16 +0200

    Comments and placing in ovl_en_dns

--- ASM/src/endns_checkydist.asm
+++ ASM/src/endns_checkydist.asm
@@ -1,16 +1,12 @@
-; z_en_dns
 EnDns_CheckYDist:
+    lui     at,0x42c8           ; 100.0f
+    mtc1    at,$f4              ; Load 100.0f to float register
+    lwc1    $f0,148(s0)         ; EnDns Y distance to player
+    abs.s   $f0,$f0             ; Absolute EnDns-player Y distance
+    c.lt.s  $f0,$f4             ; Y distance < 100.0f?
+    bc1tl   @@EnDns_YReturn     ; If yes, continue function
+    nop                         ; Else...
+    addi    ra,ra,0x8c          ; Go to 0x80a7550c (EnDns_Idle function return)
+@@EnDns_YReturn:
+    jr      ra                  ; Unchanged ra at 0x80a75598
     move    a0,s0
-    lui     at,0x42c8
-    lwc1    $f0,148(s0)
-    mtc1    at,$f4
-    abs.s   $f0,$f0
-    c.lt.s  $f0,$f4
-    bc1fl   @@EnDns_OutsideRange
-    nop                     ; in Y talking range, continue function
-    jr      ra              ; 0x80a7550c
-    nop
-@@EnDns_OutsideRange:       ; outside of Y talking range, return function
-    addi    ra,ra,0x8c
-    jr      ra              ; 0x80a75598
-    nop
