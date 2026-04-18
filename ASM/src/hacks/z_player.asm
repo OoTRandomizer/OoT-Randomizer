62b5a3ee27dee2478d7d14d7f7380cee2751b07f
djevangelia <djevangelia>
2026-04-18 18:05:37 +0200

    Call Matrix_Push and Matrix_Pop when drawing Bunny Hood and Hover Boots hover

---
+++ ASM/src/hacks/z_player.asm
@@ -0,0 +1,28 @@
+.headersize(0x808301c0 - 0xbcdb70)
+
+;================================================================================
+; Call Matrix_Push() and Matrix_Pop() when drawing Bunny Hood and Hover Boots
+; hover effect, to avoid very big frozen drawing if frozen
+;================================================================================
+; Replaces lw      v0,704(s1)
+;          lui     t8,0xdb06
+.org 0x808482f8
+    jal     BunnyMatrixPush
+    nop
+
+; Replaces lw      v0,704(s1)
+.org 0x808483b4
+    jal     BunnyMatrixPop
+    ;lui    t3,0xde00          ; Needs to be here because it's a branch target address
+
+; Replaces lwc1    $f12,36(s0)
+;          lw      a2,44(s0)
+.org 0x8084852c
+    jal     HoverMatrixPush
+    nop
+
+; Replaces sw      v0,4(s0)
+;          lw      v1,720(s1)
+.org 0x80848578
+    jal     HoverMatrixPop
+    sw      v0,4(s0)
---
+++ ASM/src/player_bunny_hover_matrix.asm
@@ -0,0 +1,48 @@
+
+BunnyMatrixPush:
+    addiu   sp,sp,-12
+    sw      ra,4(sp)
+    sw      a0,8(sp)    ; Matrix_Push uses a0, a1, need to save them
+    jal     Matrix_Push
+    sw      a1,12(sp)
+    lw      ra,4(sp)
+    lw      a0,8(sp)
+    lw      a1,12(sp)
+    lui     t8,0xdb06   ; Displaced
+    lw      v0,704(s1)  ; Displaced
+    jr      ra
+    addiu   sp,sp,12
+
+BunnyMatrixPop:
+    addiu   sp,sp,-4
+    sw      ra,4(sp)    ; No need to save registers here
+    jal     Matrix_Pop
+    nop
+    lw      ra,4(sp)
+    lw      v0,704(s1)  ; Displaced
+    jr      ra
+    addiu   sp,sp,4
+
+HoverMatrixPush:
+    addiu   sp,sp,-12
+    sw      ra,4(sp)
+    sw      a0,8(sp)    ; Matrix_Push uses a0, a1, need to save them
+    jal     Matrix_Push
+    sw      a1,12(sp)
+    lw      ra,4(sp)
+    lw      a0,8(sp)
+    lw      a1,12(sp)
+    lwc1    $f12,36(s0) ; Displaced
+    lw      a2,44(s0)   ; Displaced
+    jr      ra
+    addiu   sp,sp,12
+
+HoverMatrixPop:
+    addiu   sp,sp,-4
+    sw      ra,4(sp)    ; No need to save registers here
+    jal     Matrix_Pop
+    nop
+    lw      ra,4(sp) 
+    lw      v1,720(s1)  ; Displaced
+    jr      ra
+    addiu   sp,sp,4
