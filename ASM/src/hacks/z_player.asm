4c705f01a4b9cdeced4967fdd9782d6c6a66ae3e
djevangelia <djevangelia>
2026-05-09 10:31:25 +0200

    Prevent softlock when Hookshot cannot spawn on equip
    Hook into Player_InitHookshotIA
    Asm check if Hookshot actor is NULL
    If so, Player_UseItem item none

--- ASM/src/hacks/z_player.asm
+++ ASM/src/hacks/z_player.asm
@@ -6,5 +6,15 @@
 ; Replaces  lw  t8,1644(s0)
 ;           lui at,0xffdf
 .org 0x8084a6c4         ; in Player_Action_DismountLadder (0x803a3064)
-    jal  player_ladder_cutscene
+    jal     Player_LadderCutsceneFix
     nop
+
+;================================================================================
+; Prevent softlock if Hookshot actor cannot spawn when equipping
+; (memory shortage due to Hyrule Field glitch, child equip etc)
+;================================================================================
+; Replaces  lw      a1,60(sp)
+;           sw      v0,924(a1)
+.org 0x808319d4         ; in Player_InitHookshotIA (0x8038a374)
+     jal     Player_HookshotCheckActorSpawn
+     lw      a1,60(sp)       ; displaced (loads player)
