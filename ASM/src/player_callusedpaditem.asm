e220ad78d2d6f7a450b0b0c8fc3c37d6cac0a4af
djevangelia <djevangelia>
2026-05-11 20:56:29 +0200

    Player_CallUseDpadItem proper abi

--- ASM/src/player_callusedpaditem.asm
+++ ASM/src/player_callusedpaditem.asm
@@ -1,17 +1,17 @@
 ; Call C function for using item on D-pad
 Player_CallUseDpadItem:
-    addiu   sp,sp,-8
-    sw      ra,(sp)
+    addiu   sp,sp,-24
+    sw      ra,16(sp)
     jal     Player_UseDpadItem        ; return in v0: 1 if used D-pad item, else 0
-    sw      v1,4(sp)                  ; needs to be saved
+    sw      v1,20(sp)                 ; needs to be saved
     beqz    v0,@@DidntUseReturn       ; if didn't use item, continue normal function
-    lw      ra,(sp)
+    lw      ra,16(sp)
     b       @@DidUseReturn            ; else, go to 0x80832134 (end of calling function)
     addi    ra,0x74
 @@DidntUseReturn:
-    lw      v1,4(sp)                  ; restore for continue function
+    lw      v1,20(sp)                 ; restore for continue function
     move    a3,zero                   ; displaced
     li      a1,4                      ; displaced
 @@DidUseReturn:
     jr      ra
-    addiu   sp,sp,8
+    addiu   sp,sp,24
