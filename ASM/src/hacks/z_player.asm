6fbc1bcffd4bd5e1257785a4adbebaa46164dbe1
djevangelia <djevangelia>
2026-05-11 21:24:03 +0200

    Player_MatrixPush/Pop proper abi

--- ASM/src/hacks/z_player.asm
+++ ASM/src/hacks/z_player.asm
@@ -1,4 +1,4 @@
-.headersize(0x808301c0 - 0xbcdb70)
+.headersize (0x808301c0 - 0xbcdb70)
 
 ;================================================================================
 ; Call Matrix_Push() and Matrix_Pop() when drawing Bunny Hood and Hover Boots
@@ -7,22 +7,22 @@
 ; Replaces lw      v0,704(s1)
 ;          lui     t8,0xdb06
 .org 0x808482f8
-    jal     BunnyMatrixPush
+    jal     Player_BunnyMatrixPush
     nop
 
 ; Replaces lw      v0,704(s1)
 .org 0x808483b4
-    jal     BunnyMatrixPop
+    jal     Player_BunnyMatrixPop
     ;lui    t3,0xde00          ; Needs to be here because it's a branch target address
 
 ; Replaces lwc1    $f12,36(s0)
 ;          lw      a2,44(s0)
 .org 0x8084852c
-    jal     HoverMatrixPush
+    jal     Player_HoverMatrixPush
     nop
 
 ; Replaces sw      v0,4(s0)
 ;          lw      v1,720(s1)
 .org 0x80848578
-    jal     HoverMatrixPop
+    jal     Player_HoverMatrixPop
     sw      v0,4(s0)
--- ASM/src/player_bunny_hover_matrix.asm
+++ ASM/src/player_bunny_hover_matrix.asm
@@ -1,47 +1,47 @@
-BunnyMatrixPush:
-    addiu   sp,sp,-12
-    sw      ra,4(sp)
-    sw      a0,8(sp)    ; Matrix_Push uses a0, a1, need to save them
+Player_BunnyMatrixPush:
+    addiu   sp,sp,-24
+    sw      ra,16(sp)
+    sw      a0,20(sp)    ; Matrix_Push uses a0, a1, need to save them
     jal     Matrix_Push
-    sw      a1,12(sp)
-    lw      ra,4(sp)
-    lw      a0,8(sp)
-    lw      a1,12(sp)
+    sw      a1,24(sp)
+    lw      ra,16(sp)
+    lw      a0,20(sp)
+    lw      a1,24(sp)
     lui     t8,0xdb06   ; Displaced
     lw      v0,704(s1)  ; Displaced
     jr      ra
-    addiu   sp,sp,12
+    addiu   sp,sp,24
 
-BunnyMatrixPop:
-    addiu   sp,sp,-4
-    sw      ra,4(sp)    ; No need to save registers here
+Player_BunnyMatrixPop:
+    addiu   sp,sp,-24
+    sw      ra,16(sp)    ; No need to save registers here
     jal     Matrix_Pop
     nop
-    lw      ra,4(sp)
+    lw      ra,16(sp)
     lw      v0,704(s1)  ; Displaced
     jr      ra
-    addiu   sp,sp,4
+    addiu   sp,sp,24
 
-HoverMatrixPush:
-    addiu   sp,sp,-12
-    sw      ra,4(sp)
-    sw      a0,8(sp)    ; Matrix_Push uses a0, a1, need to save them
+Player_HoverMatrixPush:
+    addiu   sp,sp,-24
+    sw      ra,16(sp)
+    sw      a0,20(sp)    ; Matrix_Push uses a0, a1, need to save them
     jal     Matrix_Push
-    sw      a1,12(sp)
-    lw      ra,4(sp)
-    lw      a0,8(sp)
-    lw      a1,12(sp)
+    sw      a1,24(sp)
+    lw      ra,16(sp)
+    lw      a0,20(sp)
+    lw      a1,24(sp)
     lwc1    $f12,36(s0) ; Displaced
     lw      a2,44(s0)   ; Displaced
     jr      ra
-    addiu   sp,sp,12
+    addiu   sp,sp,24
 
-HoverMatrixPop:
-    addiu   sp,sp,-4
-    sw      ra,4(sp)    ; No need to save registers here
+Player_HoverMatrixPop:
+    addiu   sp,sp,-24
+    sw      ra,16(sp)    ; No need to save registers here
     jal     Matrix_Pop
     nop
-    lw      ra,4(sp)
+    lw      ra,16(sp)
     lw      v1,720(s1)  ; Displaced
     jr      ra
-    addiu   sp,sp,4
+    addiu   sp,sp,24
