fe9e8f81f318d5a1f911f2df2527dfe9e77b4c94
djevangelia <djevangelia>
2026-05-27 12:43:17 +0200

    Goron Link talkstate fix, remove ra manipulation

--- ASM/src/hacks/z_en_go2.asm
+++ ASM/src/hacks/z_en_go2.asm
@@ -1,11 +1,15 @@
-.headersize(0x80b56910 - 0x00ed2040)
+.headersize (0x80b56910 - 0x00ed2040)
 
 ;================================================================================
 ; Prevent Goron Link first talk softlock if out of range. Add talkState ==
 ; NPC_TALK_STATE_TALKING as a condition by itself to update talkState and set textId
 ;================================================================================
-; Replaces  lw  a0,32(sp)
+; Replaces: jal Npc_TrackPoint
+;           sw  a0,32(sp)
+;           lw  a0,32(sp)
 ;           lw  v0,384(a0)
-.org 0x80b58f10         ; in func_80A45288
+.org 0x80b58f08         ; in func_80A45288
     jal     EnGo2_TalkstateUpdate
+    sw      a0,32(sp)       ; displaced
+    bnez    t0,0x80b58f3c   ; if talking, run update talkstate
     nop
